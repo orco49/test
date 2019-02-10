40d36e1352b538edd57500ac339b4a0b5dbfc829
or <orcohen49@gmail.com>
2019-02-10 13:20:59 +0000

    bal

---
+++ orco_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   char* name = (char *) malloc(sizeof(char)*8);
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
