9ccd1bcce7ecd6fdc941a5c270608d577e834be3
orco49 <36543310+orco49@users.noreply.github.com>
2019-09-23 16:21:29 +0300

    delete test file

--- orco_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-   char* name = (char *) malloc(sizeof(char)*8);
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
